140b51f2bcd9c0aaa5b536d85702064a93530188
Justin Worthe <justin.worthe@gmail.com>
2015-06-03 23:03:40 +0200

    Moved mapping move to string to its own file

--- src/spacebot.cpp
+++ src/spacebot.cpp
@@ -1,4 +1,5 @@
 #include "spacebot.h"
+#include "move_string_mapper.h"
 #include <random>
 #include <fstream>
 #include <string>
@@ -21,42 +22,14 @@ Move Spacebot::chooseMove()
     int max = static_cast<int>(Move::BUILD_SHIELD);
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(min, max);
+    std::uniform_int_distribution<int> dis(min, max);
     return static_cast<Move>(dis(gen));
 }
 
 void Spacebot::writeMove(const Move& move)
 {
     std::ofstream resultStream(outputFilename);
-    switch (move)
-    {
-    case Move::NOTHING:
-	resultStream << "Nothing";
-	break;
-    case Move::MOVE_LEFT:
-	resultStream << "MoveLeft";
-	break;
-    case Move::MOVE_RIGHT:
-	resultStream << "MoveRight";
-	break;
-    case Move::SHOOT:
-	resultStream << "Shoot";
-	break;
-    case Move::BUILD_ALIEN_FACTORY:
-	resultStream << "BuildAlienFactory";
-	break;
-    case Move::BUILD_MISSILE_CONTROLLER:
-	resultStream << "BuildMissileController";
-	break;
-    case Move::BUILD_SHIELD:
-	resultStream << "BuildShield";
-	break;
-    default:
-	resultStream << "MoveLeft";
-    }
-	 
-    resultStream << std::endl;
-    resultStream.flush();
+    resultStream << MoveStringMapper().toString(move) << std::endl;
     return;
 }
 
